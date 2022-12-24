29460993c7f5aa1bc02b3fbaa30f6e7f9ef09723
RAHUL CHITTURI <57553666+Rahul-chitturi@users.noreply.github.com>
2022-12-24 05:36:56 +0000

    dev

--- src/component/App.js
+++ src/component/App.js
@@ -5,9 +5,27 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div>
-       Hellow world
+      <div class="container">
+      <div class="mac-terminal">
+        <div class="header">
+          <div class="header__op">
+            <span class="header__op-icon header__op-icon--red"></span>
+            <span class="header__op-icon header__op-icon--yellow"></span>
+            <span class="header__op-icon header__op-icon--green"></span>
+          </div>
+          <div class="header__title">root@macbook:~/projects</div>
+          <div class="header__empty"></div>
+        </div>
+        <div class="body">
+          <div class="body__row">
+            <code>
+              ssfdf
+            </code>
+          </div>
+                  
+        </div>
       </div>
+    </div>
     );
   }
 }
