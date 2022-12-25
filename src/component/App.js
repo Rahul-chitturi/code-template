5f890e34d599b28ce95cd80e2d16816925f0633a
RAHUL CHITTURI <57553666+Rahul-chitturi@users.noreply.github.com>
2022-12-25 07:49:10 +0000

    dev

--- src/component/App.js
+++ src/component/App.js
@@ -1,29 +1,49 @@
 import React, { useEffect, useState } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
-import { styles } from "../utils";
+import { backgroundColors, styles } from "../utils";
 import "./App.css";
 
 export default function App() {
 
   const supportedLanguages = SyntaxHighlighter.supportedLanguages;
   const [language, setLanguage] = useState("javascript");
+  const [background, setBackground] = useState("Gainsboro");
   const [name, setName] = useState("HTML");
+  const [showLine, setShowLine] = useState(false);
   const [style, setStyle] = useState("a11y-dark");
   const [script, setScript] = useState("console.log('hello world!')");
-
+  
   useEffect(()=>{
-    console.log(style)
+
   }
-  , [language,style])
+  , [language,style, showLine])
   return (
-    <div className="sudo-main">
+    <div style={{background: background}}>
+    <div className="sudo-main" style={{"--backgroung-col": background}}>
         <div className="sudo-content">
-       
+
       <div className="section-one container">
+      <div className="w-full">
+      <label htmlFor="ln">Background Color</label>
+        <select
+          className="form-control"
+          value={background}
+          name="ln"
+          onChange={e => setBackground(e.target.value)}
+        >
+          {backgroundColors.map(l => (
+            <option key={l} value={l}>
+              {l}
+            </option>
+          ))}
+        </select>
+      </div>
+      <label htmlFor="showLine">Show Line Number</label>
+      <input type="checkbox"  className="form-control" value={showLine} name="showLine"  onChange={(e) => setShowLine(e.target.checked)}  />
         <label htmlFor="script">Script</label>
-      <textarea className="form-control" value={script}   onChange={e => setScript(e.target.value)} name="script" id="script"></textarea>
+      <textarea className="form-control" value={script}   onChange={(e) => setScript(e.target.value)} name="script" id="script"></textarea>
       <label htmlFor="title">Title</label>
-      <input className="form-control" name="title" placeholder="Title"></input>
+      <input className="form-control" name="title" value={name} onChange={(e) => setName(e.target.value)} placeholder="Title"></input>
       <div className="w-full">
       <label htmlFor="ln">Language</label>
         <select
@@ -68,7 +88,7 @@ export default function App() {
           </div>
           <div className="body">
             <div className="body__row">
-            <SyntaxHighlighter language={language} style={require(`react-syntax-highlighter/dist/esm/styles/hljs/${style}`).default}>
+            <SyntaxHighlighter language={language} wrapLongLines={true} wrapLines={true}  showLineNumbers={showLine} style={require(`react-syntax-highlighter/dist/esm/styles/hljs/${style}`).default}>
             {script}
             </SyntaxHighlighter>
             </div>
@@ -77,6 +97,7 @@ export default function App() {
         </div>
       </div>
       </div>
+    </div>
     </div>
   );
 }
